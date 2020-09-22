13730e9c1d36d5e36fb0a6f47f308c5a672236e0
francisco <franciscogaletti@yahoo.com>
2020-09-21 23:46:54 -0500

    Commit master, template

---
+++ 70_483_ExamRef/70_483.ConsoleApp/ParallelEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _70_483.ConsoleApp
+{
+     public  static class ParallelEntity
+    {
+        public static void Task1()
+        {
+            Console.WriteLine("Task 1 starting");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task 1 ending");
+        }
+        public static void Task2()
+        {
+            Console.WriteLine("Task 2 starting");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task 2 ending");
+        }
+
+        public static void WorkOnItem(object item)
+        {
+            Console.WriteLine("starting working on :" + item);
+           // Thread.Sleep(1000);
+            Console.WriteLine("finishing working on :" + item);
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public string City { get; set; }
+        }
+    }
+}
--- 70_483_ExamRef/70_483.ConsoleApp/Program.cs
+++ 70_483_ExamRef/70_483.ConsoleApp/Program.cs
@@ -2,20 +2,126 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _70_483.ConsoleApp
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            //Parallel.Invoke(() => Task1(), () => Task2());
+            //Console.WriteLine("finishing processing");
+            //Console.ReadKey();
+
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+
+
         }
+
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Parallel ");
+            Console.WriteLine("2) Parallel foreach");
+            Console.WriteLine("3) Parallel for");
+            Console.WriteLine("4) Parallel LINQ");
+
+            Console.WriteLine("99) EXIT");
+            Console.Write("\r\nSelect an option: ");
+
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Parallel.Invoke(() => ParallelEntity.Task1(), () => ParallelEntity.Task2());
+                    Console.WriteLine("finishing processing");
+                    Console.ReadKey();
+                    return true;
+               
+                case "2":
+                    var items = Enumerable.Range(0, 500);
+                    Parallel.ForEach(items, item =>
+                    {
+                        ParallelEntity.WorkOnItem(item);
+                    });
+                    Console.WriteLine("Finishing Processing");
+                    Console.ReadKey();
+                    return true;
+
+                case "3":
+
+                    //FOR
+                    var items2 = Enumerable.Range(0, 50).ToArray();
+                    Parallel.For(0, items2.Length, i =>
+                    {
+                        ParallelEntity.WorkOnItem(items2[i]);
+                    });
+
+                    Console.WriteLine("Finishing Processing");
+                    Console.ReadKey();
+                    return true;
+
+                case "4":
+                    ParallelEntity.Person[] people = new ParallelEntity.Person[]
+                    {
+                     new ParallelEntity.Person { Name="Alan", City="Hull"},
+                      new ParallelEntity.Person { Name="Berly", City="Seattle"},
+                       new ParallelEntity.Person { Name="Charles", City="London"},
+                     new ParallelEntity.Person { Name="David", City="Seattle"},
+                      new ParallelEntity.Person { Name="Eddy", City="Paris"},
+                       new ParallelEntity.Person { Name="Fred", City="BErlin"},
+                        new ParallelEntity.Person { Name="Grodon", City="Hulll"},
+                         new ParallelEntity.Person { Name="Henry", City="Seatle"},
+                          new ParallelEntity.Person { Name="Isaac", City="London"}
+                    };
+
+                    var result = from person in people.AsParallel()
+                                 where person.City == "Seattle"
+                                 select person;
+
+                    foreach (var person in result)
+                    {
+                        Console.WriteLine(person.Name);
+                    }
+                    Console.WriteLine("Finishing Processing");
+                    Console.ReadKey();
+                    return true;
+
+                case "99":
+                    Environment.Exit(0);
+                    return true;
+                case "99":
+                    Environment.Exit(0);
+                    return true;
+                default:
+                    return true;
+            }
+        }
+
+        //    static void Task1()
+        //{
+        //    Console.WriteLine("Task 1 starting");
+        //    Thread.Sleep(2000);
+        //    Console.WriteLine("Task 1 ending");
+        //}
+        //static void Task2()
+        //{
+        //    Console.WriteLine("Task 2 starting");
+        //    Thread.Sleep(1000);
+        //    Console.WriteLine("Task 2 ending");
+        //}
+
+
+
     }
 }
