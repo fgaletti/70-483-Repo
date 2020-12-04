dc30bbc8c4a006fdd8f1532db4d3ea0da3e095c5
francisco <franciscogaletti@yahoo.com>
2020-12-04 04:53:01 -0600

    Initial commit Types branch

--- 70_483_ExamRef/70_483.ConsoleApp/Program.cs
+++ 70_483_ExamRef/70_483.ConsoleApp/Program.cs
@@ -9,14 +9,8 @@ namespace _70_483.ConsoleApp
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
-
-            //Parallel.Invoke(() => Task1(), () => Task2());
-            //Console.WriteLine("finishing processing");
-            //Console.ReadKey();
 
             bool showMenu = true;
             while (showMenu)
@@ -31,10 +25,11 @@ namespace _70_483.ConsoleApp
         {
             Console.Clear();
             Console.WriteLine("Choose an option:");
-            Console.WriteLine("1) Parallel ");
-            Console.WriteLine("2) Parallel foreach");
-            Console.WriteLine("3) Parallel for");
-            Console.WriteLine("4) Parallel LINQ");
+            Console.WriteLine("1) Enum  ");
+            Console.WriteLine("2) Struct  ");
+            Console.WriteLine("3) Static Constructors  ");
+            Console.WriteLine("4) Extensions  ");
+            Console.WriteLine("5) Index property  ");
 
             Console.WriteLine("99) EXIT");
             Console.Write("\r\nSelect an option: ");
@@ -43,61 +38,72 @@ namespace _70_483.ConsoleApp
             switch (Console.ReadLine())
             {
                 case "1":
-                    Parallel.Invoke(() => ParallelEntity.Task1(), () => ParallelEntity.Task2());
+                    Console.WriteLine(AlineStateNormal.Destroyed); // 1
+                    Console.WriteLine(AlienState.Destroyed); // 4
+
                     Console.WriteLine("finishing processing");
                     Console.ReadKey();
                     return true;
-               
+
                 case "2":
-                    var items = Enumerable.Range(0, 500);
-                    Parallel.ForEach(items, item =>
-                    {
-                        ParallelEntity.WorkOnItem(item);
-                    });
-                    Console.WriteLine("Finishing Processing");
+
+                    Alien a;
+                    a.X = 50;
+                    a.Y = 60;
+                    a.Lives = 4;
+                    // initialize all properties
+                    Console.WriteLine("a: {0}", a.ToString());
+
+                    Alien[] swarm = new Alien[100];
+                    Console.WriteLine("x {0}" , swarm[0].ToString());
+
+                    Console.WriteLine("finishing processing");
                     Console.ReadKey();
                     return true;
 
                 case "3":
 
-                    //FOR
-                    var items2 = Enumerable.Range(0, 50).ToArray();
-                    Parallel.For(0, items2.Length, i =>
-                    {
-                        ParallelEntity.WorkOnItem(items2[i]);
-                    });
+                    //static alien class
+                    // it is called ONCE before the creation of the firts instance of the class
+                    AlienClass alien = new AlienClass(); 
+                    AlienClass alien2 = new AlienClass(); //should not print 
 
-                    Console.WriteLine("Finishing Processing");
+                    Console.WriteLine("finishing static");
                     Console.ReadKey();
                     return true;
 
                 case "4":
-                    ParallelEntity.Person[] people = new ParallelEntity.Person[]
-                    {
-                     new ParallelEntity.Person { Name="Alan", City="Hull"},
-                      new ParallelEntity.Person { Name="Berly", City="Seattle"},
-                       new ParallelEntity.Person { Name="Charles", City="London"},
-                     new ParallelEntity.Person { Name="David", City="Seattle"},
-                      new ParallelEntity.Person { Name="Eddy", City="Paris"},
-                       new ParallelEntity.Person { Name="Fred", City="BErlin"},
-                        new ParallelEntity.Person { Name="Grodon", City="Hulll"},
-                         new ParallelEntity.Person { Name="Henry", City="Seatle"},
-                          new ParallelEntity.Person { Name="Isaac", City="London"}
-                    };
 
-                    var result = from person in people.AsParallel()
-                                 where person.City == "Seattle"
-                                 select person;
+                    //extensions
+                    string text = @"a rocker ... ,
+                                    line2,
+                                     line 33,
+                                     line 4";
 
-                    foreach (var person in result)
-                    {
-                        Console.WriteLine(person.Name);
-                    }
-                    Console.WriteLine("Finishing Processing");
+                    Console.WriteLine(text.LineCount());
+
+                    //case 2
+                    int numberToString = 99;
+                    Console.WriteLine(numberToString.ToString2()); // extension 
+
+                    Console.WriteLine("finishing extensions");
                     Console.ReadKey();
                     return true;
 
-              
+                case "5":
+                    // Index Property
+
+                    IndexAccess indexClass = new IndexAccess();
+                    indexClass[2] = 22; // value passed in [] is not an array 
+                    Console.WriteLine(indexClass[2].ToString());
+
+                    // string
+                    indexClass["zero"] = 99; // value passed in [] is not an array 
+                    Console.WriteLine(indexClass["zero"].ToString());
+
+                    Console.WriteLine("finishing extensions");
+                    Console.ReadKey();
+                    return true;
                 case "99":
                     Environment.Exit(0);
                     return true;
@@ -106,20 +112,100 @@ namespace _70_483.ConsoleApp
             }
         }
 
-        //    static void Task1()
-        //{
-        //    Console.WriteLine("Task 1 starting");
-        //    Thread.Sleep(2000);
-        //    Console.WriteLine("Task 1 ending");
-        //}
-        //static void Task2()
-        //{
-        //    Console.WriteLine("Task 2 starting");
-        //    Thread.Sleep(1000);
-        //    Console.WriteLine("Task 2 ending");
-        //}
+        enum AlineStateNormal
+        {
+            Sleeping,
+            Attacking,
+            Destroyed
+        }
+        enum AlienState :
+            byte
+        {
+            Sleeping =1,
+            Attacking =2,
+            Destroyed = 4
+        };
+    }
 
+    struct Alien
+    {
+        public int X;
+        public int Y;
+        public int Lives;
 
+        public Alien(int x , int y)
+        {
+            X = x;
+            Y = y;
+            Lives = 3;
+        }
 
+        public override string ToString()
+        {
+            return string.Format("X: {0} Y: {1} Lives: {2}", X, Y, Lives);
+        }
+    }
+
+    // static constructors
+    class AlienClass
+    {
+        static AlienClass()
+        {
+            Console.WriteLine("Static Alien constructor");
+        }
+    }
+
+    //extensions
+    public static class MyExtensions
+    {
+        public static int LineCount(this String str)
+        {
+            return str.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Length; 
+        }
+
+        public static string ToString2(this int integer)
+        {
+            return integer.ToString() + ".000";
+        }
+    }
+
+    // Index Property
+    public class IndexAccess
+    {
+        private int[] indexArray = new int[100];
+
+        public int this[int i]
+        {
+            get { return indexArray[i]; }
+            set { indexArray[i] = value; }
+        }
+
+        public int this[string name] {
+            get {
+                switch (name)
+                {
+                    case "zero":
+                        return indexArray[0];
+                    case "one":
+                        return indexArray[1];
+                    default:
+                        return -1;
+                }
+            }
+            set
+            {
+                switch (name)
+                {
+                    case "one":
+                        indexArray[1] = value;
+                        break;
+                    case "zero":
+                        indexArray[0] = value;
+                        break;
+                    default:
+                        break;
+                } ;
+            }
+        }
     }
 }
