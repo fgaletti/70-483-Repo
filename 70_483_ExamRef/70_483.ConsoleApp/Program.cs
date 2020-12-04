b84733e4db5978258d243faca69c977a42f9e5c0
fgalettilivingwd <franciscogaletti@yahoo.com>
2020-12-03 18:05:56 -0600

    Exceptions

--- 70_483_ExamRef/70_483.ConsoleApp/Program.cs
+++ 70_483_ExamRef/70_483.ConsoleApp/Program.cs
@@ -9,32 +9,21 @@ namespace _70_483.ConsoleApp
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
-
-            //Parallel.Invoke(() => Task1(), () => Task2());
-            //Console.WriteLine("finishing processing");
-            //Console.ReadKey();
-
             bool showMenu = true;
             while (showMenu)
             {
                 showMenu = MainMenu();
             }
-
-
         }
 
         private static bool MainMenu()
         {
             Console.Clear();
             Console.WriteLine("Choose an option:");
-            Console.WriteLine("1) Parallel ");
-            Console.WriteLine("2) Parallel foreach");
-            Console.WriteLine("3) Parallel for");
-            Console.WriteLine("4) Parallel LINQ");
+            Console.WriteLine("1) Custom ");
+            Console.WriteLine("2) Inner ");
 
             Console.WriteLine("99) EXIT");
             Console.Write("\r\nSelect an option: ");
@@ -43,61 +32,57 @@ namespace _70_483.ConsoleApp
             switch (Console.ReadLine())
             {
                 case "1":
-                    Parallel.Invoke(() => ParallelEntity.Task1(), () => ParallelEntity.Task2());
+
+                    try
+                    {
+                        throw new ClassCalcException("Div Zero", ClassCalcException.CalcErrores.DivisionByZero);
+                    }
+                    catch (ClassCalcException ce) when (ce.Error == ClassCalcException.CalcErrores.DivisionByZero)
+                    {
+                        Console.WriteLine("Error: {0}", ce.Error);
+                    }
+                    catch (ClassCalcException ex)
+                    {
+                        Console.WriteLine("Error: {0}", ex.Error);
+                    }
+
                     Console.WriteLine("finishing processing");
                     Console.ReadKey();
                     return true;
-               
+
                 case "2":
-                    var items = Enumerable.Range(0, 500);
-                    Parallel.ForEach(items, item =>
+
+                    try
                     {
-                        ParallelEntity.WorkOnItem(item);
-                    });
-                    Console.WriteLine("Finishing Processing");
-                    Console.ReadKey();
-                    return true;
+                        try
+                        {
+                            Console.Write("Enter an integer: ");
+                            string numberText = Console.ReadLine();
+                            int result;
+                            result = int.Parse(numberText);
+                        }
+                        catch   (FormatException fx)
+                        {
+                            throw new Exception("Calculator failure", fx);
+                        }
+                        catch (Exception ex)
 
-                case "3":
-
-                    //FOR
-                    var items2 = Enumerable.Range(0, 50).ToArray();
-                    Parallel.For(0, items2.Length, i =>
-                    {
-                        ParallelEntity.WorkOnItem(items2[i]);
-                    });
-
-                    Console.WriteLine("Finishing Processing");
-                    Console.ReadKey();
-                    return true;
-
-                case "4":
-                    ParallelEntity.Person[] people = new ParallelEntity.Person[]
-                    {
-                     new ParallelEntity.Person { Name="Alan", City="Hull"},
-                      new ParallelEntity.Person { Name="Berly", City="Seattle"},
-                       new ParallelEntity.Person { Name="Charles", City="London"},
-                     new ParallelEntity.Person { Name="David", City="Seattle"},
-                      new ParallelEntity.Person { Name="Eddy", City="Paris"},
-                       new ParallelEntity.Person { Name="Fred", City="BErlin"},
-                        new ParallelEntity.Person { Name="Grodon", City="Hulll"},
-                         new ParallelEntity.Person { Name="Henry", City="Seatle"},
-                          new ParallelEntity.Person { Name="Isaac", City="London"}
-                    };
-
-                    var result = from person in people.AsParallel()
-                                 where person.City == "Seattle"
-                                 select person;
-
-                    foreach (var person in result)
-                    {
-                        Console.WriteLine(person.Name);
+                        {
+                            var a = ex.GetType().Name;
+                            throw new Exception("Calculator failure", ex);
+                        }
                     }
-                    Console.WriteLine("Finishing Processing");
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Console.WriteLine(ex.InnerException.Message);
+                        Console.WriteLine(ex.InnerException.StackTrace);
+                    }
+            
+                    Console.WriteLine("finishing processing");
                     Console.ReadKey();
                     return true;
 
-              
                 case "99":
                     Environment.Exit(0);
                     return true;
@@ -106,20 +91,20 @@ namespace _70_483.ConsoleApp
             }
         }
 
-        //    static void Task1()
-        //{
-        //    Console.WriteLine("Task 1 starting");
-        //    Thread.Sleep(2000);
-        //    Console.WriteLine("Task 1 ending");
-        //}
-        //static void Task2()
-        //{
-        //    Console.WriteLine("Task 2 starting");
-        //    Thread.Sleep(1000);
-        //    Console.WriteLine("Task 2 ending");
-        //}
+    }
 
-
+    class ClassCalcException : Exception
+    {
+        public enum CalcErrores
+        {
+            InvalidNumberText,
+            DivisionByZero
+        }
+        public CalcErrores Error { get; set; }
+        public ClassCalcException (string message, CalcErrores error) :base(message)
+        {
+            Error = error;
+        }
 
     }
 }
