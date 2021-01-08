e3013d1d70a3ef8f89eac143edeb37d9d50bc12b
francisco <franciscogaletti@yahoo.com>
2021-01-08 09:41:37 -0600

    Add branch Exam

--- 70_483_ExamRef/70_483.ConsoleApp/Program.cs
+++ 70_483_ExamRef/70_483.ConsoleApp/Program.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+using System.Runtime.Serialization.Json;
 namespace _70_483.ConsoleApp
 {
     class Program
@@ -22,16 +24,57 @@ namespace _70_483.ConsoleApp
         {
             Console.Clear();
             Console.WriteLine("Choose an option:");
-            Console.WriteLine("1)  ");
+            Console.WriteLine("1)  Delete Rows list");
+            Console.WriteLine("2)  Dictionary");
+            Console.WriteLine("3)  DatAjSON"); // Console.WriteLine("2)  Dictionary");
             Console.WriteLine("99) EXIT");
             Console.Write("\r\nSelect an option: ");
            
             switch (Console.ReadLine())
             {
                 case "1":
+                    List<int> listInts = new List<int>();
+                    listInts.Add(1);
+                    listInts.Add(2);
+                    listInts.Add(3);
+                    listInts.Add(4);
+
+                    // remove
+                    for (int i = 0; i < listInts.Count ; i++)
+                    {
+                        listInts.RemoveAt(0);
+                    }
+               
+
+                    Console.WriteLine("finishing processing Remove list");
+                    Console.ReadKey();
+                    return true;
+                case "2":
+                    //dictionary
+                    Dictionary<int, string> dict = new Dictionary<int, string>();
+                    dict.Add(0, "cero");
+                    dict.Add(1, "uno");
                     
 
-                    Console.WriteLine("finishing processing");
+                    ConcurrentDictionary<int, string> condict = new ConcurrentDictionary<int, string>();
+
+                    condict.TryAdd(0, "cero");
+                    condict.TryAdd(1, "uno");
+
+                    string valorEliminar;
+                    condict.TryRemove(1, out valorEliminar);
+
+
+                    Console.WriteLine("finishing processing Remove list");
+                    Console.ReadKey();
+                    return true;
+
+                case "2":
+                    //dictionary
+                    var ser = new DataContractJsonSerializer(typeof(Dog));
+
+
+                    Console.WriteLine("finishing processing Remove list");
                     Console.ReadKey();
                     return true;
                 case "99":
